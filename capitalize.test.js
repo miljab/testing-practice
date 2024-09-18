1c9cc11584f3cd96c450248d0ddbf82a8d071d89
miljab <jablonskimilosz1337@gmail.com>
2024-09-18 16:10:40 +0200

    Add more tests for capitalize func

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,3 +1,15 @@
 import { capitalize } from "./capitalize";
 
 it("lowercase string", () => expect(capitalize("abc")).toBe("Abc"));
+
+it("numbers", () => expect(capitalize("123")).toBe("123"));
+
+it("upper and lowercase string", () =>
+  expect(capitalize("aaBadCa")).toBe("AaBadCa"));
+
+it("uppercase string", () => expect(capitalize("AAAA")).toBe("AAAA"));
+
+it("full sentence", () =>
+  expect(
+    capitalize("lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+  ).toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit."));
